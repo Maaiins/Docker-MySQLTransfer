6d801ec3e10ab6deb98229268596d29e4cecfaae
Nicolai Lauser <n.lauser@polytec.de>
2016-09-20 13:38:05 +0200

    Separated dump and import

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -139,7 +139,8 @@ Begin transaction $(date)
 
 EOT
 
-mysqldump -v --user="${MYSQL_SOURCE_USER}" --password="${MYSQL_SOURCE_PASSWORD}" --host="${MYSQL_SOURCE_ADDRESS}" --port="${MYSQL_SOURCE_PORT}" --triggers "${MYSQL_SOURCE_DATABASE}" | mysql --user="${MYSQL_TARGET_USER}" --password="${MYSQL_TARGET_PASSWORD}" --host="${MYSQL_TARGET_ADDRESS}" --port="${MYSQL_TARGET_PORT}" "${MYSQL_TARGET_DATABASE}"
+mysqldump -v --user="${MYSQL_SOURCE_USER}" --password="${MYSQL_SOURCE_PASSWORD}" --host="${MYSQL_SOURCE_ADDRESS}" --port="${MYSQL_SOURCE_PORT}" --routines --triggers "${MYSQL_SOURCE_DATABASE}" > /dump.sql
+mysql --user="${MYSQL_TARGET_USER}" --password="${MYSQL_TARGET_PASSWORD}" --host="${MYSQL_TARGET_ADDRESS}" --port="${MYSQL_TARGET_PORT}" "${MYSQL_TARGET_DATABASE}" < /dump.sql
 
 cat >&1 <<-EOT
 
