84e8c349e6a75b2a965a44fc5ddb6fbccc219bd0
Nicolai Lauser <n.lauser@polytec.de>
2016-09-20 12:07:48 +0200

    Added force command

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -139,7 +139,7 @@ Begin transaction $(date)
 
 EOT
 
-mysqldump --user="${MYSQL_SOURCE_USER}" --password="${MYSQL_SOURCE_PASSWORD}" --host="${MYSQL_SOURCE_ADDRESS}" --port="${MYSQL_SOURCE_PORT}" --routines --triggers "${MYSQL_SOURCE_DATABASE}" | mysql --user="${MYSQL_TARGET_USER}" --password="${MYSQL_TARGET_PASSWORD}" --host="${MYSQL_TARGET_ADDRESS}" --port="${MYSQL_TARGET_PORT}" "${MYSQL_TARGET_DATABASE}"
+mysqldump -fv --user="${MYSQL_SOURCE_USER}" --password="${MYSQL_SOURCE_PASSWORD}" --host="${MYSQL_SOURCE_ADDRESS}" --port="${MYSQL_SOURCE_PORT}" --routines --triggers "${MYSQL_SOURCE_DATABASE}" 1>&1 | mysql --user="${MYSQL_TARGET_USER}" --password="${MYSQL_TARGET_PASSWORD}" --host="${MYSQL_TARGET_ADDRESS}" --port="${MYSQL_TARGET_PORT}" "${MYSQL_TARGET_DATABASE}"
 
 cat >&1 <<-EOT
 
